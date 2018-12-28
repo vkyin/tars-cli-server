13e8106d806a38676af41a3b53a6b5f1f89ccb5c
vkyin(殷咏骐) <vkyin@tencent.com>
2018-12-28 18:06:19 +0800

    update

--- test.js
+++ test.js
@@ -1,35 +1,23 @@
 const WebSocket = require('ws')
-const { UploadCodeZip } = require('./lib/protocol/command').Command
+const Context = require('./lib/Context')
+const cmdHandler = require('./lib/commandHandle')
 
-function heartbeat () {
-  clearTimeout(this.pingTimeout)
+const client = new WebSocket('ws://127.0.0.1:8080/')
 
-  // Use `WebSocket#terminate()` and not `WebSocket#close()`. Delay should be
-  // equal to the interval at which your server sends out pings plus a
-  // conservative assumption of the latency.
-  this.pingTimeout = setTimeout(() => {
-    this.terminate()
-  }, 30000 + 1000)
-}
+const ctx = new Context(client)
 
-const client = new WebSocket('ws://localhost:8080/')
-
-client.on('open', heartbeat)
-client.on('ping', heartbeat)
-client.on('close', function clear () {
-  clearTimeout(this.pingTimeout)
+client.on('message', async function (message) {
+  try {
+    const [cmd, cmdData] = cmdHandler.deserialize(ctx, message)
+    await cmdHandler[cmd](ctx, ctx.state, cmdData)
+  } catch (error) {
+    console.log(error)
+  }
 })
 
-client.on('message', d => console.log(d))
-
 setTimeout(() => {
-  const { TarsDeploy } = require('./lib/protocol/command').Command
-
-  const c = new TarsDeploy()
-  c.readFromObject({
-    serverName: 'vktest',
+  ctx.send('SaveFile', {
+    fileName: 'vktest',
     filebuff: require('fs').readFileSync('./test.zip')
   })
-  const msg = Buffer.concat([Buffer.from('TarsDeploy\0'), c.toBinBuffer().toNodeBuffer()])
-  client.send(msg)
 }, 1000)
